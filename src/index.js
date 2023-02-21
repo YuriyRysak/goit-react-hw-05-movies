edbfed4d6eadfc6ef5daff35e774b54760fda8a1
YuriyRysak <yuriy.risak@gmail.com>
2023-02-21 11:34:22 +0200

    basename add

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </StrictMode>
