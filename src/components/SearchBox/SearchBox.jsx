66df7ed857074dec9e31ccf0ae0b018ff7f46b21
YuriyRysak <yuriy.risak@gmail.com>
2023-03-06 18:04:56 +0200

    change files movie end searchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,29 +1,20 @@
-import { MovieList} from '../../components/MovieList/MovieList';
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { fetchByQuery } from '../../api';
+import { useState} from 'react';
+
 import { Wrapper, Input, Button, SearchForm, Icon } from "./SearchBox.styled";
 
-export const SearchBox = () => {
+export const SearchBox = ( {onSubmit} ) => {
   const [query, setQuery] = useState('');
-  const [movies, setMovies] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams('');
-  
 
-  const searchQuery = searchParams.get('query');
-
-  useEffect(() => {
-    searchQuery && fetchByQuery(searchQuery).then(setMovies);
-  }, [searchQuery]);
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    setSearchParams({ query });
-    setQuery('');
-  };
-  const onChange = e => {
+   const onChange = e => {
     setQuery(e.target.value);
   };
+   
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit(query)
+    setQuery('');
+  };
+ 
 
   return (
     <>
@@ -31,8 +22,6 @@ export const SearchBox = () => {
       <SearchForm onSubmit={handleSubmit}>
       <Input
         type="text"
-        autocomplete="off"
-        autofocus="off"
         placeholder="Search movies..."
         value={query}
         onChange={onChange}
@@ -41,7 +30,7 @@ export const SearchBox = () => {
       <Button type="submit"><Icon/></Button>
       </SearchForm>
     </Wrapper>
-    <MovieList movies={movies} />
+    
 
    </>
   );
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,14 +1,37 @@
+import { MovieList} from '../../components/MovieList/MovieList';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByQuery } from '../../api';
 import { SearchBox } from "../../components/SearchBox/SearchBox";
 
 import { FormDiv } from './Movies.styled';
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams('');
+
   
+
+  useEffect(() => {
+    const query = searchParams.get('query');
+
+    query && fetchByQuery(query).then(setMovies);
+  }, [searchParams]);
+
+  const onHandleSubmit = query => {
+   
+    setSearchParams( {query} );
+    // console.log(query);
+    
+    
+  };
+
   return (
     <>
       <FormDiv>
-      <SearchBox />
+      <SearchBox onSubmit={onHandleSubmit}/>
       </FormDiv>
+      <MovieList movies={movies} />
       
     </>
   );
