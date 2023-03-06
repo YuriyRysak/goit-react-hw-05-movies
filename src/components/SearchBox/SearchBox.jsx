52fe7161ee802ba22499753a8405b36d4e20d5cd
YuriyRysak <yuriy.risak@gmail.com>
2023-03-06 13:06:49 +0200

    change file SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,22 +1,48 @@
-// import { Form } from "react-router-dom";
+import { MovieList} from '../../components/MovieList/MovieList';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByQuery } from '../../api';
 import { Wrapper, Input, Button, SearchForm, Icon } from "./SearchBox.styled";
 
-export const SearchBox = ({ value, onChange, onSubmit }) => {
+export const SearchBox = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams('');
+  
+
+  const searchQuery = searchParams.get('query');
+
+  useEffect(() => {
+    searchQuery && fetchByQuery(searchQuery).then(setMovies);
+  }, [searchQuery]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setSearchParams({ query });
+    setQuery('');
+  };
+  const onChange = e => {
+    setQuery(e.target.value);
+  };
+
   return (
+    <>
     <Wrapper>
-      
-      <SearchForm onSubmit={onSubmit}>
+      <SearchForm onSubmit={handleSubmit}>
       <Input
         type="text"
         autocomplete="off"
         autofocus="off"
         placeholder="Search movies..."
-        value={value}
+        value={query}
         onChange={onChange}
        />
        
       <Button type="submit"><Icon/></Button>
       </SearchForm>
     </Wrapper>
+    <MovieList movies={movies} />
+
+   </>
   );
 };
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,44 +1,15 @@
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { fetchByQuery } from '../../api';
 import { SearchBox } from "../../components/SearchBox/SearchBox";
-import { MovieList} from '../../components/MovieList/MovieList';
 
-import {
-  FormDiv,
-} from './Movies.styled';
+import { FormDiv } from './Movies.styled';
 
 const Movies = () => {
-  const [query, setQuery] = useState('');
-  const [movies, setMovies] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams('');
   
-
-  const searchQuery = searchParams.get('query');
-
-  useEffect(() => {
-    searchQuery && fetchByQuery(searchQuery).then(setMovies);
-  }, [searchQuery]);
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    const response = await fetchByQuery(query);
-    setMovies(response);
-    setSearchParams({ query });
-    setQuery('');
-  };
-  const onChange = e => {
-    setQuery(e.target.value);
-  };
-
   return (
     <>
       <FormDiv>
-      <SearchBox value={query} onChange={onChange} onSubmit={handleSubmit}/>
+      <SearchBox />
       </FormDiv>
-      <MovieList movies={movies} />
-     
+      
     </>
   );
 };
